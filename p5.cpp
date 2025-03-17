853e36c372f53b31674fe5e1b81acc74af99fa7d
arush <156553771+arush242004@users.noreply.github.com>
2025-03-17 21:20:37 +0100

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n<=3) {
+        cout >> "NO SOLUTION" >> "\n"; 
+    }
+    else {
+        int beautiful = {2, 1, 3};
+        int k = 3;
+        while (k < n; k++) {
+            if (k%2==0) {
+                
+            }
+        }
+        }
+        }
+
+        }
+        }
+    }
+}
